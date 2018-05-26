5ac66b6ae0bf97ba2e257aa71c8ad9fcf46ec0ee
Steven Blom <atomicblom@binaryvibrance.net>
2018-05-26 23:10:36 +0930

    Fix transforms

--- src/main/java/com/github/atomicblom/weirdinggadget/client/opengex/OpenGEXModelInstance.java
+++ src/main/java/com/github/atomicblom/weirdinggadget/client/opengex/OpenGEXModelInstance.java
@@ -10,12 +10,15 @@ import com.google.common.collect.ImmutableMap;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.renderer.block.model.BakedQuad;
 import net.minecraft.client.renderer.block.model.IBakedModel;
+import net.minecraft.client.renderer.block.model.ItemCameraTransforms;
 import net.minecraft.client.renderer.block.model.ItemCameraTransforms.TransformType;
 import net.minecraft.client.renderer.block.model.ItemOverrideList;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.client.renderer.vertex.VertexFormat;
 import net.minecraft.util.EnumFacing;
+import net.minecraftforge.client.ForgeHooksClient;
 import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.client.model.PerspectiveMapWrapper;
 import net.minecraftforge.client.model.pipeline.LightUtil;
 import net.minecraftforge.client.model.pipeline.UnpackedBakedQuad;
 import net.minecraftforge.common.model.IModelPart;
@@ -347,5 +350,12 @@ public class OpenGEXModelInstance implements IBakedModel
     {
         // TODO handle items
         return ItemOverrideList.NONE;
+    }
+
+
+    @Override
+    public Pair<? extends IBakedModel, Matrix4f> handlePerspective(TransformType cameraTransformType)
+    {
+        return PerspectiveMapWrapper.handlePerspective(this, state, cameraTransformType);
     }
 }
