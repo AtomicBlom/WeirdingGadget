bf1331d92a8c51b5d6ff71d4dcee5f1fc004099f
Steven Blom <atomicblom@binaryvibrance.net>
2019-05-09 00:22:24 +0930

    Work around foamfix anarchy + ctmlib 3.3

--- src/main/java/com/github/atomicblom/weirdinggadget/client/opengex/OpenGEXModelLoader.java
+++ src/main/java/com/github/atomicblom/weirdinggadget/client/opengex/OpenGEXModelLoader.java
@@ -1,5 +1,6 @@
 package com.github.atomicblom.weirdinggadget.client.opengex;
 
+import com.github.atomicblom.weirdinggadget.Logger;
 import com.github.atomicblom.weirdinggadget.client.opengex.ogex.*;
 import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
@@ -10,13 +11,23 @@ import net.minecraftforge.client.model.ICustomModelLoader;
 import net.minecraftforge.client.model.IModel;
 import net.minecraftforge.client.model.ModelLoaderRegistry;
 import net.minecraftforge.fml.common.FMLLog;
+import net.minecraftforge.fml.common.Loader;
+import net.minecraftforge.fml.common.ModContainer;
+import net.minecraftforge.fml.common.versioning.ArtifactVersion;
+import net.minecraftforge.fml.common.versioning.DefaultArtifactVersion;
+import net.minecraftforge.fml.common.versioning.VersionRange;
 import org.apache.logging.log4j.Level;
+import scala.tools.nsc.Global;
+
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
+import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
+import java.util.stream.Stream;
 
 public enum OpenGEXModelLoader implements ICustomModelLoader {
     INSTANCE;
@@ -90,12 +101,85 @@ public enum OpenGEXModelLoader implements ICustomModelLoader {
 
 
         if (scene.getMetrics().getUp() == Axis.Z) {
-            float[] cycleMatrix = {
+            boolean useNormalCycleMatrix = true;
+
+            ModContainer ctm = null;
+            ModContainer foamfix = null;
+
+            for (ModContainer modContainer : Loader.instance().getModList()) {
+                if ("ctm".equals(modContainer.getModId())) {
+                    ctm = modContainer;
+                    Logger.info(ctm.toString());
+                } else if ("foamfix".equals(modContainer.getModId())) {
+                    foamfix = modContainer;
+                    Logger.info(foamfix.toString());
+                }
+                if (foamfix != null && ctm != null) break;
+            }
+
+            try {
+                if (ctm != null && foamfix != null) {
+                    Logger.warning("Detected ctm and foamfix, we might need to hack the model");
+                    //First detect if we have a bad version of CTM
+                    //Chisel versions have the format of MC<version>-<chiselversion>
+                    String ctmVersionString = ctm.getVersion();
+                    if (ctmVersionString.contains("-")) {
+                        int dashOffset = ctmVersionString.indexOf("-") + 1;
+                        ctmVersionString = ctmVersionString.substring(dashOffset);
+                    }
+                    ArtifactVersion ctmVersion = new DefaultArtifactVersion(ctmVersionString);
+                    VersionRange badVersions;
+                    try {
+                        badVersions = VersionRange.createFromVersionSpec("[0.3.3,)");
+                    } catch (Exception e) {
+                        throw new RuntimeException("Well that didn't work");
+                    }
+
+                    if (badVersions.containsVersion(ctmVersion)) {
+                        Logger.warning("Look out, we have CTM > v3.3, better check for foamfix anarchy.");
+
+                        try {
+                            Class foamFixShared = Class.forName("pl.asie.foamfix.shared.FoamFixShared");
+                            boolean isCoreMod = foamFixShared.getField("isCoremod").getBoolean(null);
+                            Object foamFixConfig = foamFixShared.getField("config").get(null);
+                            boolean clWipeModelCache = foamFixConfig.getClass().getField("clWipeModelCache").getBoolean(foamFixConfig);
+
+                            if (isCoreMod && clWipeModelCache) {
+                                Logger.warning("Detected dastardly combo of foamfix anarchy + ctm 2.3, using alternative rotations");
+                                useNormalCycleMatrix = false;
+                            } else {
+                                Logger.info("Foamfix was not configured as a coremod or clWipeModelCache was not enabled.");
+                            }
+                        } catch (Exception e) {
+                            Logger.warning(e.toString());
+                            Logger.warning("Nevermind, we couldn't find FoamFixShared.");
+                        }
+
+                    } else {
+                        Logger.warning("Nope. It's an older version of ctm lib. *phew*");
+                    }
+                }
+            } catch (Exception e) {
+                Logger.severe("There was an error detecting if we needed to work around a ctm 3.3/foamfix anarchy bug.");
+                Logger.severe(e.toString());
+            }
+
+
+            float[] foamFixCycleMatrix = {
+                    0, 1, 0, 0,
+                    0, 0, 1, 0,
+                    1, 0, 0, 0,
+                    0, 0, 0, 1
+            };
+
+            float[] normalcycleMatrix = {
                     0, 0, 1, 0,
                     1, 0, 0, 0,
                     0, 1, 0, 0,
                     0, 0, 0, 1
             };
+
+            float[] cycleMatrix = useNormalCycleMatrix ? normalcycleMatrix : foamFixCycleMatrix;
 
             for (final OgexNode ogexNode : scene)
             {
